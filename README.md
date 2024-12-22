22e9d19b36732a27b79116ddeaf4807dfb1714f2
Ronald <tzaerk.18@gmail.com>
2024-12-22 15:22:21 -0400

    Listo el paso 15

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 Para correr el proyecto:
 
 1. Abrir la terminal y ubicarse en la carpeta del proyecto.
-3. escribir en la terminal npm install 
-4. escribir en la terminal npm install express
-5. escribir en la terminal npm install cors
-6. escribir en la terminal npm install dotenv 
+3. Escribir en la terminal npm install 
+4. Escribir en la terminal npm install express
+5. Escribir en la terminal npm install cors
+6. Escribir en la terminal npm install dotenv 
+7. Escribir en la terminal npm install mongoose 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 const app = express()
 dotenv.config()
@@ -11,6 +12,7 @@ app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, function() {
+    connectDB()
     console.log(`Api corriendo en http://localhost:${port} !`)
 })
 
@@ -18,3 +20,22 @@ app.get('/', (req, res) =>{
     console.log('Mi primer endponit')
     res.status(200).send('Hola mundo, mi primera API!')
 })
+
+const connectDB = () => {
+
+    const {
+        MONGO_USERNAME,
+        MONGO_PASSWORD,
+        MONGO_HOSTNAME,
+        MONGO_PORT,
+        MONGO_DB
+    } = process.env
+
+    const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
+
+    mongoose.connect(url).then( function() {
+        console.log('MongoDB is connected')
+    }).catch( function(err) {
+        console.log(err)
+    })
+}
